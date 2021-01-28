9a3ddb3add4e5ac893cc63299b4474c35ac7a4d6
Illrin <kelvin.mai101@yahoo.com>
2021-01-28 10:26:06 -0800

    Finished Sleep Input
    Sleep input now functional, calculates total sleep time and gets sleep quality. Doesn't save it to anywhere, no database ATM.
    Also got rid of the floatin button for now.

--- app/src/main/java/com/example/sleeperstudent/SleepInput.java
+++ app/src/main/java/com/example/sleeperstudent/SleepInput.java
@@ -1,18 +1,24 @@
 package com.example.sleeperstudent;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import org.joda.time.Period;
 import org.joda.time.PeriodType;
@@ -24,12 +30,15 @@ import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SleepInput#newInstance} factory method to
+ * Use the {@link SleepInput} factory method to
  * create an instance of this fragment.
  */
 public class SleepInput extends Fragment {
-    Button btStartDate, btEndDate, btSubmit;
+    Button btStartDate, btEndDate, btSubmit, btStartTime, btEndTime;
     DatePickerDialog.OnDateSetListener dateSetListener1, dateSetListener2;
+    int startHour, startMinute, endHour, endMinute;
+
+    SeekBar sbMood, sbQuality, sbStress;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +51,12 @@ public class SleepInput extends Fragment {
         btStartDate = view.findViewById(R.id.bt_startDate);
         btEndDate = view.findViewById(R.id.bt_endDate);
         btSubmit = view.findViewById(R.id.bt_submit);
+        btStartTime = view.findViewById(R.id.bt_startTime);
+        btEndTime = view.findViewById(R.id.bt_endTime);
+
+        sbMood = view.findViewById(R.id.sb_mood);
+        sbQuality = view.findViewById(R.id.sb_quality);
+        sbStress = view.findViewById(R.id.sb_stress);
 
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
@@ -49,7 +64,7 @@ public class SleepInput extends Fragment {
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         //Set dates as today
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
         String date = simpleDateFormat.format(Calendar.getInstance().getTime());
         btEndDate.setText(date);
         btStartDate.setText(date);
@@ -70,7 +85,7 @@ public class SleepInput extends Fragment {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month = month + 1;
-                String date = dayOfMonth + "/" + month + "/" + year;
+                String date = month + "/" + dayOfMonth + "/" + year;
                 btStartDate.setText(date);
             }
         };
@@ -86,21 +101,64 @@ public class SleepInput extends Fragment {
                 datePickerDialog.show();
             }
         });
+
         dateSetListener2 = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month = month + 1;
-                String date = dayOfMonth + "/" + month + "/" + year;
+                String date = month + "/" + dayOfMonth + "/" + year;
                 btEndDate.setText(date);
             }
         };
 
-        btSubmit.setOnClickListener(new View.OnClickListener() {
+        btStartTime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        getActivity(),
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                startHour = hourOfDay;
+                                startMinute = minute;
+                                Calendar calendar = Calendar.getInstance();
+                                calendar.set(0,0,0,startHour,startMinute);
+                                btStartTime.setText(DateFormat.format("hh:mm aa", calendar));
+                            }
+                        }, 12, 0, false);
+                timePickerDialog.updateTime(startHour,startMinute);
+                timePickerDialog.show();
+            }
+        });
+
+        btEndTime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        getActivity(),
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                endHour = hourOfDay;
+                                endMinute = minute;
+                                Calendar calendar = Calendar.getInstance();
+                                calendar.set(0,0,0,endHour,endMinute);
+                                btEndTime.setText(DateFormat.format("hh:mm aa", calendar));
+                            }
+                        }, 12, 0, false);
+                timePickerDialog.updateTime(endHour,endMinute);
+                timePickerDialog.show();
+            }
+        });
+
+        btSubmit.setOnClickListener(new View.OnClickListener() { //Submission button, calculate
+            //time slept and quality of sleep, put in db
             @Override
             public void onClick(View view) {
                 String sDate = btStartDate.getText().toString();
                 String eDate = btEndDate.getText().toString();
-                SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("dd/MM/yyyy");
+                SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("MM/dd/yyyy");
+                int sleepTime = 0; //time slept, in minutes
                 try {
                     Date date1 = simpleDateFormat1.parse(sDate);
                     Date date2 = simpleDateFormat1.parse(eDate);
@@ -111,10 +169,35 @@ public class SleepInput extends Fragment {
                         int years = period.getYears();
                         int months = period.getMonths();
                         int days = period.getDays();
+                        if(startHour > endHour){
+                            days--;
+                            endHour += 24;
+                        }
+                        if(startMinute > endMinute) {
+                            endHour--;
+                            endMinute += 60;
+                        }
+                        if(days >= 0) sleepTime += ((endHour - startHour) * 60)
+                                + (endMinute - startMinute) + (days * 24 * 60);
                     }
+                    //sleep quality
+                    int mood = sbMood.getProgress();
+                    int quality = sbQuality.getProgress();
+                    int stress = sbQuality.getProgress();
+
+                    //TODO put into database
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
+            }
+        });
+
+        //transition to first screen
+        view.findViewById(R.id.bt_inputToFirst).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SleepInput.this)
+                        .navigate(R.id.action_sleepInput_to_FirstFragment);
             }
         });
     }
